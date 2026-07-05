829e6f52c2e01c38fadef61b65d137340c7cd87a
kayo joel <kayo@kali.kayo.com>
2026-07-05 21:20:41 +0100

    configuration du projet pour deployement

--- employerProject/settings.py
+++ employerProject/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from email.policy import default
 from pathlib import Path
+from decouple import config,Csv
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5w#*#w=!yy#(8z&k#q%hs-j@+-r9-)_dh9jv+wu+&$^=cw=v95'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
 ALLOWED_HOSTS = []
 
@@ -74,12 +77,18 @@ WSGI_APPLICATION = 'employerProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+    DATABASES = {
+     'default': {
+         dj_database_url.config(default=config('DATABASE_URL'))
+     }
 }
+
 
 
 # Password validation
